e6ddf4077fec5a6570b2a1d0c343c930f65a92ee
Alberto Contorno <alberto.contorno@gmail.com>
2018-05-13 00:50:10 +0200

    new chain functions

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,15 @@
-blockchain = [1]
+blockchain = [[1]]
 
-def add_value():
-    blockchain.append([ blockchain[0], 5.3])
-    print(blockchain)
 
-add_value()
-add_value()
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount):
+    blockchain.append([get_last_blockchain_value(), transaction_amount])
+
+add_value(5.3)
+add_value(4)
+add_value(2)
+
+print(blockchain)
