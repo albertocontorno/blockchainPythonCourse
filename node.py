b0acd52fd60fec9256c1149ef09d9ecf9774199c
Alberto Contorno <alberto.contorno@gmail.com>
2018-05-22 22:28:35 +0200

    aggiunti setter(getter blockchain

--- node.py
+++ node.py
@@ -5,10 +5,10 @@ from uuid import uuid4
 from verification import Verification
 class Node:
 
-    def __init__(self, blockchain):
+    def __init__(self):
         #self.id = str(uuid4())
         self.id= 'MySelf'
-        self.blockchain = blockchain(self.id)
+        self.blockchain = Blockchain(self.id)
 
     def get_transaction_value(self):
         """ Returns the input of the user (a new transaction amount) as a float """
@@ -37,7 +37,6 @@ class Node:
             print("2: Min a new block")
             print("3: Output the blockchain blocks")
             print("5: Check transaction validity")
-            print("h: Manipulate the chain")
             print("q: To quit")
             user_choice = self.get_user_choice()
             if(user_choice == "1"):
@@ -47,30 +46,21 @@ class Node:
                     print("Added transaction!")
                 else:
                     print("Transaction failed")
-                print(self.blockchain.open_transactions)
+                print(self.blockchain.get_open_transactions())
             elif(user_choice == "2"):
-                self.blockchain.mine_block():
+                self.blockchain.mine_block()
             elif(user_choice == "3"):
                 self.print_blockchainelements()
-            elif(user_choice == "4"):
-                verifier = Verification()
-                if verifier.verify_transactions(self.blockchain.open_transactions, self.blockchain.get_balance):
+            elif(user_choice == "4"): 
+                if Verification.verify_transactions(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
                     print("All transactions are valid")
                 else:
                     print("A transaction is invalid")
-            elif(user_choice == "h"):
-                if len(self.blockchain) > 0:
-                    self.blockchain[0] = {
-                        "previous_hash": "",
-                        "index": 0,
-                        "transactions": [{"sender": "Chris", "recipient": "Max", "amount": 100}]
-                    }
             elif(user_choice == "q"):
                 waiting_for_input = False
             else:
                 print("Input was invalid, please pick a value from the list")
-            verifier = Verification()
-            if verifier.verify_chain(self.blockchain.chain) == False:
+            if Verification.verify_chain(self.blockchain.chain) == False:
                 print("invalid blockchain")
                 waiting_for_input = False
             print('Balance of {}: {:6.2f}'.format(self.id, self.blockchain.get_balance()))
