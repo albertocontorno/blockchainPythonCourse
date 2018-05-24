e745e48b55a75d221427c96e0c7e1e1d72dcd2f5
Alberto Contorno <alberto.contorno@gmail.com>
2018-05-24 23:13:01 +0200

    aggiunta signature e verifica nelle transazioni

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,7 @@ from block import Block
 from transaction import Transaction
 from utility.hash_util import hash_block
 from utility.verification import Verification
+from wallet import Wallet
 MINING_REWARD = 10
 class Blockchain:
     def __init__(self, hosting_node_id):
@@ -42,7 +43,7 @@ class Blockchain:
                 blockchain = json.loads(file_content[0][:-1])
                 updated_blockchain = []
                 for block in blockchain:
-                    converted_tx = [Transaction(tx["sender"], tx["recipient"], tx["amount"]) for tx in block["transactions"]]
+                    converted_tx = [Transaction(tx["sender"], tx["recipient"], tx["signature"], tx["amount"]) for tx in block["transactions"]]
                     #converted_tx = [OrderedDict([("sender", tx["sender"]), ("recipient", tx["recipient"]), ("amount", tx["amount"])]) for tx in block["transactions"]]
 
                     updated_block = Block(block["previous_hash"], block["index"], converted_tx, block["proof"], block["timestrap"])
@@ -53,10 +54,10 @@ class Blockchain:
                 open_transactions = json.loads(file_content[1])
                 updated_transactions = []
                 for tx in open_transactions:
-                    updated_transaction = Transaction(tx["sender"], tx["recipient"], tx["amount"])
+                    updated_transaction = Transaction(tx["sender"], tx["recipient"], tx["signature"], tx["amount"])
                     #updated_transaction = OrderedDict([("sender", tx["sender"]), ("recipient", tx["recipient"]), ("amount", tx["amount"])])
                     updated_transactions.append(updated_transaction)
-                self.__open_transactionsansactions = updated_transactions
+                self.__open_transactions = updated_transactions
         except (IOError, IndexError):
             print("Handled exception")
         finally:
@@ -68,7 +69,7 @@ class Blockchain:
                 saveable_chain = [block.__dict__ for block in [Block(block_el.previous_hash, block_el.index, [tx.__dict__ for tx in block_el.transactions] , block_el.proof, block_el.timestrap) for block_el in self.__chain]]
                 f.write(json.dumps(saveable_chain))
                 f.write("\n")
-                saveable_tx = [tx.__dict__.copy() for tx in self.__open_transactionsansactions]
+                saveable_tx = [tx.__dict__.copy() for tx in self.__open_transactions]
                 f.write(json.dumps(saveable_tx))
                 # save_data = {
                 #     "chain": blockchain,
@@ -83,7 +84,7 @@ class Blockchain:
         last_block = self.__chain[-1]
         last_hash = hash_block(last_block)
         proof = 0
-        while(not Verification.valid_proof(self.__open_transactionsansactions, last_hash, proof)):
+        while(not Verification.valid_proof(self.__open_transactions, last_hash, proof)):
             proof += 1
         return proof
 
@@ -93,7 +94,7 @@ class Blockchain:
 
         tx_sender = [[tx.amount for tx in block.transactions
                     if tx.sender == partecipant] for block in self.__chain]
-        open_tx_sender = [tx.amount for tx in self.__open_transactionsansactions if tx.sender == partecipant]
+        open_tx_sender = [tx.amount for tx in self.__open_transactions if tx.sender == partecipant]
         tx_sender.append(open_tx_sender)
         amount_sent = 0
         amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
@@ -118,18 +119,22 @@ class Blockchain:
 
 
 
-    def add_transaction(self, recipient, sender, amount=1.0):
+    def add_transaction(self, recipient, sender, signature, amount=1.0,):
         """ Append a new value as well as the last blockchain value to the blockchain.
         Arguments:
             :sender: The sender of the coins that should be addeed.
             :recipient: The recipient of the coins 
             :param: amount: The amount of coins sent with the transaction (default = 1.0)
         """
+        print("NODEEE, ",self.hosting_node)
+        if self.hosting_node == None:
+            return False
         # transaction = {"sender": sender, "recipient": recipient, "amount": amount}
-        transaction = Transaction(sender, recipient, amount)
+        transaction = Transaction(sender, recipient, signature, amount)
+        
         #transaction = OrderedDict([("sender", sender), ("recipient", recipient), ("amount", amount)])
         if(Verification.verify_transaction(transaction, self.get_balance)):
-            self.__open_transactionsansactions.append(transaction)
+            self.__open_transactions.append(transaction)
             #partecipants.add(sender)
             #partecipants.add(recipient)
             self.save_data()
@@ -138,6 +143,8 @@ class Blockchain:
 
 
     def mine_block(self):
+        if self.hosting_node == None:
+            return False
         print("BLOCKCHAIN ", self.__chain)
         last_block = self.__chain[-1]
         # for key in last_block:
@@ -150,14 +157,18 @@ class Blockchain:
         #     "recipient": owner,
         #     "amount": MINING_REWARD
         # }
-        reward_transaction = Transaction("MINING", self.hosting_node, MINING_REWARD)
+        reward_transaction = Transaction("MINING", self.hosting_node, '', MINING_REWARD)
         #reward_transaction = OrderedDict([("sender", "MINING"), ("recipient", owner), ("amount", MINING_REWARD)])
 
-        copied_transactions = self.__open_transactionsansactions[:]
+        copied_transactions = self.__open_transactions[:]
+        for tx in block.transactions:
+            if not Wallet.verify_transaction(tx):
+                return False
         copied_transactions.append(reward_transaction)
         block = Block(hashed_block, len(self.__chain),  copied_transactions, proof)
+
         self.__chain.append(block)
-        self.__open_transactionsansactions = []
+        self.__open_transactions = []
         self.save_data()
         return True
 
